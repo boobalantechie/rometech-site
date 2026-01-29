a3e259f9cb20f70dd4fddb6ca669e8466a02c631
boobalan k <boobalantechno@gmail.com>
2026-01-29 23:43:41 +0530

    Added website files

---
+++ header.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title> vvtech </title>
+	<!-- responsive meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- For IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- master stylesheet -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Responsive stylesheet -->
+	<link rel="stylesheet" href="css/responsive.css">
+    <!--Color Switcher Mockup-->
+    <link rel="stylesheet" href="css/color-switcher-design.css">
+    <!--Color Themes-->
+    <link rel="stylesheet" href="css/color-themes/default-theme.css" id="theme-color-file">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="images/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16">
+</head>
+
+<body>
+<div class="boxed_wrapper">
+
+<div class="preloader"></div> 
+
+<!-- Start Top Bar style1 -->  
+<!-- <section class="top-bar-style2">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12">
+                <div class="top-bar clearfix">
+                    <ul class="topmenu float-left">
+                        <li><a href="#">Professional</a></li>
+                        <li><a href="#">Personal</a></li>
+                        <li><a href="#">Corporate</a></li>
+                    </ul>
+                    <div class="top-right-v2 float-right">
+                        <ul>
+                            <li><a href="#"><span class="flaticon-mail"></span>Mail Us</a></li>
+                            <li><a href="#"><span class="flaticon-pin"></span>Our Locations</a></li>
+                        </ul> -->
+                        <!-- <div class="language-switcher float-right">
+                            <div id="polyglotLanguageSwitcher">
+                                <form action="#">
+                                    <select id="polyglot-language-options">
+                                        <option id="en" value="en" selected>EN</option>
+                                        <option id="fr" value="fr">FR</option>
+                                        <option id="de" value="de">GE</option>
+                                        <option id="it" value="it">IT</option>
+                                        <option id="es" value="es">SP</option>
+                                    </select>
+                                </form>
+                            </div>
+                        </div> -->
+                    <!-- </div>
+                    
+                </div>    
+            </div>
+        </div>
+    </div>
+</section> -->
+<!-- End Top Bar style1 -->  
+ 
+<!--Start header style2 area-->
+<header class="header-style2-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12">
+                <div class="inner-content clearfix">
+                    <div class="header-style2-logo float-left">
+                        <a href="index.html">
+                            <img src="images/resources/rtlogo.jpg" alt="Awesome Logo" style="width:100px;height:100px;">
+                        </a>
+                    </div>   
+                    <div class="headers2-header-right float-right">
+                        <ul>
+                            <li>
+                                <div class="single-item">
+                                    <p><span class="flaticon-clock"></span>Opening Hrs</p>
+                                    <span>Mon-Sat: 9.00am to 6.00pm</span>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="single-item">
+                                    <p><span class="flaticon-global"></span>Address</p>
+                                    <span>192/1,chinneri vayalkadu,pallapatty,<br>new bustand,salem-636009</span>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="single-item">
+                                    <h3>Helpline:<span>+91 9042036946</span></h3>
+                                </div>
+                            </li>
+                        </ul>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>        
+</header>  
+<!--End header style2 area-->
+  
+<!--Start mainmenu area style2-->
+<section class="mainmenu-area style2 stricky">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="inner-content clearfix">
+                    <nav class="main-menu clearfix">
+                        <div class="navbar-header clearfix">   	
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li class="dropdown current"><a href="index.php">Home</a>
+                                    <!-- <ul> -->
+                                        <!-- <li><a href="index.html">Home Page - HVAC</a></li>
+                                        <li><a href="index.php">Home Page - Computer</a></li>
+                                        <li><a href="index-3.html">Home Page - Phone</a></li>
+                                    </ul> -->
+                                </li>
+                                <li class="dropdown"><a href="#about">about</a>
+                                    <!-- <ul>
+                                        <li><a href="about.html">About Our Company</a></li>
+                                        <li><a href="faq.html">FAQ’s</a></li>
+                                        <li><a href="testimonials.html">Testimonials</a></li>
+                                        <li><a href="locations.html">Locations</a></li>
+                                    </ul> -->
+                                </li>
+                                <!-- <li class="dropdown"><a href="services.html">Services</a> -->
+                                <li class="dropdown"><a href="#projects">projects</a>
+                                    <!-- <ul>
+                                        <li><a href="ser-installation.html">Installation</a></li>
+                                        <li><a href="ser-maintenance.html">Maintenance</a></li>
+                                        <li><a href="ser-repair.html">Repair</a></li>
+                                        <li><a href="ser-replacement.html">Replacement</a></li>
+                                    </ul> -->
+                                </li>
+                                <!-- <li><a href="pricing.html">Pricing</a></li> -->
+                                <!-- <li class="dropdown"><a href="blog.html">Blog</a>
+                                    <ul>
+                                        <li><a href="blog.html">Blog Default</a></li>
+                                        <li><a href="blog-large.html">Blog Large</a></li>
+                                        <li><a href="blog-single.html">Blog Single Post</a></li>
+                                    </ul>
+                                </li> -->
+                                <li class=""><a href="#gallery">gallery</a>
+                                    <!-- <ul>
+                                        <li><a href="shop.html">Shop Products</a></li>
+                                        <li><a href="shop-single.html">Products Single</a></li>
+                                        <li><a href="shopping-cart.html">Shopping Cart</a></li>
+                                        <li><a href="checkout.html">Checkout</a></li>
+                                        <li><a href="account.html">My Account</a></li>
+                                    </ul> -->
+                                </li>
+                                <li><a href="#contact">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <!-- <div class="outer-search-box">
+                        <div class="seach-toggle"><i class="fa fa-search"></i></div>
+                        <ul class="search-box">
+                            <li>
+                                <form method="post" action="http://st.ourhtmldemo.com/new/FixZone/index.php">
+                                    <div class="form-group">
+                                        <input type="search" name="search" placeholder="Search Here" required>
+                                        <button type="submit"><i class="fa fa-search"></i></button>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </div> -->
+                    <div class="button">
+                        <a href="#"><span class="flaticon-calendar-1"></span>contact us</a>
+                    </div>     
+                </div>
+            </div>
+        </div>
+    </div>
+</section>                 
+<!--End mainmenu area style2-->   
